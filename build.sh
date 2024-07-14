2a27df69b185f5c6f8703df79c4cdcd20f8b7c54
dantesieg <bahadirb@me.com>
2024-07-14 22:12:42 +0300

    kek

--- build.sh
+++ build.sh
@@ -2,22 +2,41 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
+#set -o errexit  # Exit immediately if a command exits with a non-zero status
+#set -o nounset  # Treat unset variables as an error when substituting
+#set -o pipefail # Prevents errors in a pipeline from being masked
+#set -o xtrace   # Print commands and their arguments as they are executed
 
+# Find RPM packages in the /tmp/akmods-rpms/ directory
+find /tmp/akmods-rpms/
 
-### Install packages
+# Modify the PRETTY_NAME in the os-release file
+sed -i '/^PRETTY_NAME/s/Kinoite/beblito-cachy/' /usr/lib/os-release
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Install necessary packages
+rpm-ostree install dnf5 binutils lz4
+dnf5 upgrade -y
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+# Install Nvidia drivers and related packages
+rpm-ostree install \
+    libva-nvidia-driver \
+    mesa-vulkan-drivers.i686 \
+    nvidia-driver \
+    nvidia-driver-cuda \
+    nvidia-driver-cuda-libs.i686 \
+    nvidia-driver-libs.i686 \
+    nvidia-driver-NvFBCOpenGL \
+    nvidia-modprobe \
+    nvidia-persistenced \
+    nvidia-settings \
+    /tmp/akmods-rpms/kmod-nvidia-*.rpm
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# Extract and format the qualified kernel version
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-cachy-(\d+\.\d+\.\d+)' | sed -E 's/kernel-cachy-//')"
 
-#### Example for enabling a System Unit File
+# Generate initramfs image
+/usr/libexec/rpm-ostree/wrapped/dracut --strip --aggressive-strip --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 
-systemctl enable podman.socket
+# Check for installed Nvidia kernel module
+rpm -qa | grep nvidia-kmod
